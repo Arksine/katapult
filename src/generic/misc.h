ed2bf3e8b072cd0c4f849ee69018a59cb132d6b5
Kevin O'Connor <kevin@koconnor.net>
2022-07-27 23:48:05 -0400

    bootentry: Move set_bootup_code() signatures to misc.h
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/generic/misc.h
+++ src/generic/misc.h
@@ -8,6 +8,10 @@ struct command_encoder;
 void console_sendf(const struct command_encoder *ce, va_list args);
 void *console_receive_buffer(void);
 
+#define CANBOOT_SIGNATURE 0x21746f6f426e6143 // CanBoot!
+#define REQUEST_CANBOOT 0x5984E3FA6CA1589B
+#define REQUEST_START_APP 0x7b06ec45a9a8243d
+
 uint64_t get_bootup_code(void);
 void set_bootup_code(uint64_t code);
 void application_read_flash(uint32_t address, uint32_t *dest);
