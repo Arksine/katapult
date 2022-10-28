0a24cd4502c5a027f19bbb6e507915cace9b9e11
sh83 <malishev@gmail.com>
2022-10-28 23:30:44 +0300

    rp2040: added option to force bootloader before new application flashed
    Signed-off-by: Alex Malishev <malishev@gmail.com>

---
+++ scripts/uf2_append_boot_signature.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# Add boot signature (one page of zeros) at gived address.
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+import sys, argparse, struct
+
+def renumerate(content):
+    result = bytearray()
+    total = len(content) // 512
+    for current in range(total):
+       block = content[current * 512 : (current + 1) * 512]
+       result.extend(block[ : 20])
+       result.extend(struct.pack("<II", current, total))
+       result.extend(block[ 28 : ])
+#      print("address " + hex(int.from_bytes(block[12:16], "little")))
+    return result
+
+def add_signature(content, address):
+    block = content[:512]
+    nblock = bytearray()
+    nblock.extend(block[:12])
+    nblock.extend(struct.pack("<I", address))
+    nblock.extend(block[16:32])
+    nblock.extend(bytearray(476))
+    nblock.extend(block[508:512])
+    result = bytearray()
+    result.extend(content)
+    result.extend(nblock)
+    return result
+
+def main():
+    parser = argparse.ArgumentParser(description="Merge multiple uf2")
+    parser.add_argument("-o", "--output", required=True, help="Output file")
+    parser.add_argument("-i", "--input",  required=True, help="Input files")
+    parser.add_argument("-a", "--address",  required=True, help="Address to put signature")
+
+    args = parser.parse_args()
+    address = int(args.address,0)
+    with open(args.input, 'rb') as f:
+       content = f.read()
+    content = add_signature(content, address)
+    content = renumerate(content)
+    with open(args.output, 'wb') as f:
+       f.write(content)
+       f.close()
+
+if __name__ == '__main__':
+    main()
