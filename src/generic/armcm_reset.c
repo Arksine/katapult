41067c6a742ed9466c05ed1d90b42cecbdd66ae6
Kevin O'Connor <kevin@koconnor.net>
2022-12-14 21:02:33 -0500

    armcm_reset: Rename CONFIG_BOOTLOADER_START to CONFIG_FLASH_BOOT_ADDRESS
    Rename the symbol for better consistency with the upstream Klipper
    code.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/generic/armcm_reset.c
+++ src/generic/armcm_reset.c
@@ -4,7 +4,7 @@
 //
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
-#include "autoconf.h" // CONFIG_FLASH_START
+#include "autoconf.h" // CONFIG_FLASH_BOOT_ADDRESS
 #include "board/internal.h" // NVIC_SystemReset
 #include "board/irq.h" // irq_disable
 #include "board/misc.h" // try_request_canboot
@@ -12,7 +12,7 @@
 void
 try_request_canboot(void)
 {
-    uint32_t *bl_vectors = (uint32_t *)(CONFIG_BOOTLOADER_START);
+    uint32_t *bl_vectors = (uint32_t *)(CONFIG_FLASH_BOOT_ADDRESS);
     uint64_t *req_sig = (uint64_t *)bl_vectors[0];
     irq_disable();
     *req_sig = REQUEST_CANBOOT;
